bee52df31be5ac8300c896006f948ceeec5359ad
Проць Іван <ivanprotss@gmail.com>
2021-06-19 14:19:50 +0300

    started new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      distance: 0,
+      miliradians: 0
+    }
+  this.inputChange = this.inputChange.bind(this)
+  }
+  calculateDistance = () => {
+    const oldDistance = this.state.distance;
+
+    const mil = oldDistance / 2;
+
+    this.setState({
+      miliradians: this.mil
+    })
+    console.log('1');
+  }
+
+  inputChange = (event) => {
+    this.setState({distance: event.target.value})
+    console.log(this.state);
+  }
+
+  render(){
+    return(
+    <div className='main-part'>
+      <h1>Arthillery calculator for Hell Let Loose</h1>
+      <input type='number' placeholder='Distance in mil.' value={this.state.distance} onChange={this.inputChange}></input>
+      <h2>Enter distanse in m.</h2>
+      <input type='number' placeholder='Distance in meters'></input>
+      <button type='submit' onClick={this.calculateDistance}>=</button>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
